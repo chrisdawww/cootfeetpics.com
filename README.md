f3e29f0b6e2601bcb4d66477d8879a13fbac5981
chrisdawww <chrisdawww@users.noreply.github.com>
2026-01-06 07:00:20 +0000

    deploy: 322284ad12ec78593b48ac5e38334ca394de80b3

---
+++ README.md
@@ -0,0 +1,2 @@
+# cootfeetpics.com
+A community-led effort to provide a central location for pictures of bird feet.
---
+++ eleventy.config.js
@@ -0,0 +1,17 @@
+export default function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("**/*.md");
+    eleventyConfig.addPassthroughCopy("**/*.js");
+    eleventyConfig.addPassthroughCopy("**/*.css");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+};
